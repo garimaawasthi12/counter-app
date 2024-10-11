684b029a4bcf2da13518625faa20f010899538c7
Garima <garimaawasthi11220@gmail.com>
2024-10-12 01:01:55 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 
-function App() {
-  const [value, setValue] = useState(0);
-  const [state,setState] = useState(0);
-  useEffect(()=>
-    setValue(4)
-  ,[value])
+function App(){
 
+  const [value, setValue] = useState(0);
+  
+ 
   return (
     <div className="flex flex-col gap-4 bg-gray-400 h-screen items-center justify-center">
       <div className="text-4xl">Counter App</div>
@@ -17,7 +15,7 @@ function App() {
         <div>{value}</div>
         <button onClick={()=> setValue(value+1)}>+</button>
       </div>
-      <button onClick={()=>setState(state+1)}>Change Number</button>
+   
     </div>
   );
 }
